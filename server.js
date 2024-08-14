617b54558bb13c5dccdc64a2a71445ced9253d78
Vordock <saulodias.dev@gmail.com>
2024-08-14 16:30:06 -0300

    ws

--- server.js
+++ server.js
@@ -14,3 +14,75 @@ APP.use('/favicon.ico', (req, res) => res.status(204).end());
 APP.get('/page', (req, res) => res.sendFile(path.join(__dirname, 'page/index.html')));
 APP.get('/game/1', (req, res) => res.sendFile(path.join(__dirname, 'game/1/index.html')));
 APP.get('/game/1', (req, res) => res.sendFile(path.join(__dirname, 'game/1/index.html')));
+
+let user = {
+    name: 'Mockado',
+    current_balance: 3521.00,
+    current_bet_id: '',
+    current_bet_value: 0,
+    current_multiplier: 1
+};
+
+IO_SERVER.on('connection', (socket) => {
+
+    socket.on("USER_AUTH", (received, callback) => {
+
+        callback && callback({ status: 1, data: { balance: user.balance }, message: 'Player Authenticated!' });
+
+    });
+  
+    socket.on("PLACE_BET", (emitData, callback) => {
+        console.log('\nAposta recebida:', emitData.amount);
+
+        console.log(`Saldo atual: ${user.balance}`);
+
+        const newBet_id = randomUUID();
+
+        user.bet_id = newBet_id;
+
+        console.log('    NOVO ID DE APOSTA: ', user.bet_id);
+
+        if (user.balance >= +emitData.amount) {
+            user.balance -= +emitData.amount;
+
+            console.log('\nNovo saldo total:', user.balance, '\n');
+
+            user.current_bet_value = +emitData.amount;
+
+            callback && callback({ status: 1, data:{bet: {bet_id: user.bet_id}, user:{balance: user.balance}}, message: 'Bet Successful!' });
+            user.current_multiplier = 1;
+
+        } else {
+            console.log('\nSaldo insuficiente:', user.balance, 'é menor que', emitData.amount, '\n');
+
+            callback && callback({ status: 0, message: 'Player balance is not enough.' });
+        }
+
+    });
+
+    
+    socket.on("PLACE_CASHOUT", (emitData, callback) => {
+
+        if (emitData.bet_id === user.bet_id) {
+
+            console.log('user.current_bet_value: ', user.current_bet_value);
+            console.log('user.current_multiplier: ', user.current_multiplier);
+
+            // Calcular o cashout
+            const cashout = user.current_bet_value * user.current_multiplier;
+
+            // Adicionar o cashout ao saldo do usuário
+            user.balance += cashout;
+
+            console.log(user.name, 'fez um saque de:', cashout);
+
+            console.log('\nNovo saldo:', user.balance, '\n');
+
+            callback && callback({ status: 1, data:{bet: {withdraw: cashout}, user:{balance: user.balance}}, message: 'Cashout Successful!' });
+        } else {
+            callback && callback({ status: 0, data:{bet: {bet_id: user.bet_id}, user:{balance: user.balance}}, message: 'Invalid Cashout, bet ID not found!' });
+            console.log('\nbet_id not found!\n');
+        }
+    });
+
+});
